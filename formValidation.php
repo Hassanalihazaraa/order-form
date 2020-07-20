9cbe79a98ca2d3a2fbaaf84a1259ce9ac51f1cbc
Hassan <hassanalihazaraa@gmail.com>
2020-07-20 16:58:28 +0200

    validation almost complete

--- formValidation.php
+++ formValidation.php
@@ -2,66 +2,74 @@
 declare(strict_types=1);
 
 //errors
-$errors = [
-    'emailErr' => '',
-    'streetErr' => '',
-    'streetNumberErr' => '',
-    'cityErr' => '',
-    'zipCodeErr' => '',
-    'productsErr' => []
-];
+$emailErr = '';
+$streetErr = '';
+$streetNumberErr = '';
+$cityErr = '';
+$zipCodeErr = '';
+$productsErr = [];
 
 
 //validate function
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //validate email address
     if (empty($_POST['email'])) {
-        array_push($errors['emailErr'], '<div class="alert alert-danger" role="alert">Email required</div>');
+        $emailErr = '<div class="alert alert-danger" role="alert">Email required</div>';
+        echo $emailErr;
     } else {
         $isValidEmail = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
         if (!$isValidEmail) {
-            $validEmail = 'Please provide a valid email address';
+            $inValidEmail = '<div class="alert alert-danger" role="alert">Please provide a valid email address</div>';
+            echo $inValidEmail;
         }
         $email = testInput($isValidEmail);
         $_SESSION['email'] = $email;
     }
     //validate street name
     if (empty($_POST['street'])) {
-        $streetErr = 'Street name is required';
+        $streetErr = '<div class="alert alert-danger" role="alert">Street name is required</div>';
+        echo $streetErr;
     } else {
         $street = testInput($_POST['street']);
         if (!preg_match("/^[a-zA-Z ]*$/", $street)) {
-            $streetErr = "Only letters and white space allowed";
+            $isString = '<div class="alert alert-danger" role="alert">Only letters and white space allowed</div>';
+            echo $isString;
         }
         $_SESSION['street'] = $street;
     }
     //validate street number
     if (empty($_POST['streetnumber'])) {
-        $streetNumberErr = 'Street number is required';
+        $streetNumberErr = '<div class="alert alert-danger" role="alert">Street number is required</div>';
+        echo $streetNumberErr;
     } else {
         $streetNumber = testInput($_POST['streetnumber']);
         if (!is_numeric($streetNumber)) {
-            $streetErr = "Only numbers allowed";
+            $isNan = '<div class="alert alert-danger" role="alert">Only numbers allowed</div>';
+            echo $isNan;
         }
         $_SESSION['streetnumber'] = $streetNumber;
     }
     //validate city
     if (empty($_POST['city'])) {
-        $cityErr = 'City name is required';
+        $cityErr = '<div class="alert alert-danger" role="alert">City name is required</div>';
+        echo $cityErr;
     } else {
         $city = testInput($_POST['city']);
         if (!preg_match("/^[a-zA-Z ]+$/", $city)) {
-            $streetErr = "Only letters allowed";
+            $isValidString = '<div class="alert alert-danger" role="alert">Only letters allowed</div>';
+            echo $isValidString;
         }
         $_SESSION['city'] = $city;
     }
     //validate zipcode
     if (empty($_POST['zipcode'])) {
-        $zipCodeErr = 'Zipcode is required';
+        $zipCodeErr = '<div class="alert alert-danger" role="alert">Zipcode is required</div>';
+        echo $zipCodeErr;
     } else {
         $zipCode = testInput($_POST['zipcode']);
         if (!is_numeric($zipCode)) {
-            $streetErr = "Only numbers allowed";
+            $isNanZip = '<div class="alert alert-danger" role="alert">Only numbers allowed</div>';
+            echo $isNanZip;
         }
         $_SESSION['zipcode'] = $zipCode;
     }
